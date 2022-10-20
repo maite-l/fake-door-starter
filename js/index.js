4e2d1fb9e9e1526f5c68632941a2db915aa859a0
maite.l <maite.lejeune@student.howest.be>
2022-10-20 10:07:24 +0200

    added confetti

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
+import JSConfetti from '../node_modules/js-confetti/dist/es/index.js';
+//temporary thing, this wont work on netlify
+
+
 
 const init = () => {
   const $form = document.querySelector("form");
+  const jsConfetti = new JSConfetti()
+  
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
+    jsConfetti.addConfetti();
   });
 }
 
